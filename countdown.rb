d5908a87dbdca01a00b98ac031e6fe15e3c957eb
theo <teddyhoover@gmail.com>
2020-12-07 19:35:41 -0600

    commit tree d75f7f380bf9688b2fc0b8098a9d6c36bda2446a

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown sec
+    while sec > 0
+        puts "#{sec} SECOND(S)!"
+        sec -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep sec
+    while sec > 0
+        puts "#{sec} SECOND(S)!"
+        sec -= 1
+        sleep 1
+    end
+    "HAPPY NEW YEAR!"
 end
